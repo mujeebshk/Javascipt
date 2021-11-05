c1a36d14b1b4327489103120a420ee3eb89675ce
Abdul Mujeeb <91489970+mujeebshk@users.noreply.github.com>
2021-11-05 22:18:55 +0530

    String Methods...

--- Javascript/day-2.js
+++ Javascript/day-2.js
@@ -32,4 +32,7 @@ console.log(firstName.match('e'))
 console.log (firstName.repeat(10))
 console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
 console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
-fs
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3]);
+// Only change code below this line
