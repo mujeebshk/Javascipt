a0d5b888383976b4abefbb941210e202002ac368
Abdul Mujeeb <91489970+mujeebshk@users.noreply.github.com>
2021-11-17 17:30:34 +0530

    Various scopes, Objects

---
+++ Javascript/object.js
@@ -0,0 +1,195 @@
+// Window-scope.js
+a = 'JavaScript' // is a window scope this found anywhere
+b = 10 // this is a window scope variable
+function letsLearnScope() {
+  console.log(a, b)
+  if (true) {
+    console.log(a, b)
+  }
+}
+console.log(a, b) // accessible
+
+//Global - scope.js
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  if (true) {
+    let a = 'Python'
+    let b = 100
+    console.log(a, b) // Python 100
+  }
+  console.log(a, b)
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
+
+//Local - scope.js
+let a = 'JavaScript' // is a global scope it will be found anywhere in this file
+let b = 10 // is a global scope it will be found anywhere in this file
+function letsLearnScope() {
+  console.log(a, b) // JavaScript 10, accessible
+  let c = 30
+  if (true) {
+    // we can access from the function and outside the function but 
+    // variables declared inside the if will not be accessed outside the if block
+    let a = 'Python'
+    let b = 20
+    let d = 40
+    console.log(a, b, c) // Python 20 30
+  }
+  // we can not access c because c's scope is only the if block
+  console.log(a, b) // JavaScript 10
+}
+letsLearnScope()
+console.log(a, b) // JavaScript 10, accessible
+
+//scope.js
+function letsLearnScope() {
+    var gravity = 9.81
+    console.log(gravity)
+  
+  }
+  // console.log(gravity), Uncaught ReferenceError: gravity is not defined
+  
+  if (true){
+    var gravity = 9.81
+    console.log(gravity) // 9.81
+  }
+  console.log(gravity)  // 9.81
+  
+  for(var i = 0; i < 3; i++){
+    console.log(i) // 1, 2, 3
+  }
+  console.log(i)
+
+  //scope.js
+function letsLearnScope() {
+    // you can use let or const, but gravity is constant I prefer to use const
+    const gravity = 9.81
+    console.log(gravity)
+  
+  }
+  // console.log(gravity), Uncaught ReferenceError: gravity is not defined
+  
+  if (true){
+    const  gravity = 9.81
+    console.log(gravity) // 9.81
+  }
+  // console.log(gravity), Uncaught ReferenceError: gravity is not defined
+  
+  for(let i = 0; i < 3; i++){
+    console.log(i) // 1, 2, 3
+  }
+  // console.log(i), Uncaught ReferenceError: gravity is not defined
+
+
+  // Empty object
+  const person = {}
+
+  const rectangle = {
+    length: 20,
+    width: 20
+  }
+  console.log(rectangle) // {length: 20, width: 20}
+  
+  const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    isMarried: true
+  }
+  console.log(person)
+
+  // using values
+  const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    getFullName: function() {
+      return `${this.firstName}${this.lastName}`
+    },
+    'phone number': '+3584545454545'
+  }
+  
+  // accessing values using .
+  console.log(person.firstName)
+  console.log(person.lastName)
+  console.log(person.age)
+  console.log(person.location)
+  
+  // value can be accessed using square bracket and key name
+  console.log(person['firstName'])
+  console.log(person['lastName'])
+  console.log(person['age'])
+  console.log(person['age'])
+  console.log(person['location'])
+  
+  // for instance to access the phone number we only use the square bracket method
+  console.log(person['phone number'])
+
+// object methods
+  const person = {
+    firstName: 'Asabeneh',
+    lastName: 'Yetayeh',
+    age: 250,
+    country: 'Finland',
+    city: 'Helsinki',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'React',
+      'Node',
+      'MongoDB',
+      'Python',
+      'D3.js'
+    ],
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+  
+  console.log(person.getFullName())
+  // Asabeneh Yetayeh
+
+
+
+  // has own property
+console.log(copyPerson.hasOwnProperty('name'))
+console.log(copyPerson.hasOwnProperty('score'))
+
+
+// object keys
+const keys = Object.keys(copyPerson)
+console.log(keys) //['name', 'age', 'country', 'skills', 'address', 'getPersonInfo']
+const address = Object.keys(copyPerson.address)
+console.log(address) //['street', 'pobox', 'city']
+
+// object values 
+const values = Object.values(copyPerson)
+console.log(values)
