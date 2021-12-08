3c203c9f57435e4ead1bcaa770e3621e28ad6b26
Abdul Mujeeb <91489970+mujeebshk@users.noreply.github.com>
2021-12-08 22:58:16 +0530

    CSV file reader with JS, HTML...

---
+++ CSV Reader/csv.js
@@ -0,0 +1,21 @@
+
+
+onload = fetch("/biostats.csv").then(res => {
+    return res.text()
+}).then(data => {
+    let result = data.split(/\r?\n|\r/).map(e => {
+        return e.split(",")
+    })
+    result.forEach(e => {
+        let m = e.map(e => {
+            return `<td>${e}</td>`;
+        }).join("")
+        let ce = document.createElement("tr");
+        ce.innerHTML = m;
+       if (ce.innerText != "") {
+            document.querySelector("table").appendChild(ce);
+       }
+        // console.log(m);
+
+    })
+})
