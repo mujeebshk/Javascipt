d582cf07efff9b1864a4065940b392b614393416
mujeebshk <shaikmujeeb273@gmail.com>
2021-12-15 21:51:37 +0530

    Fetching Data By Searching

--- Javascript/13.js
+++ Javascript/13.js
@@ -2,15 +2,13 @@
 // This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-export const reverseString = (string) => {
-    let strin = '';
-    for (let i = string.length-1; i >= 0 ; i-=1) {
-      strin += string[i];
-    }
-    return strin
-  };
+export const reverseSting = (string) => {
+  let strin = "";
+  for (let i = string.length - 1; i >= 0; i -= 1) {
+    strin += string[i];
+  }
+  return strin;
+};
 
-
-  //         second
-  export const reverseString = (string) =>
-  string.split('').reverse().join('');
+//         second
+export const reverseString = (string) => string.split("").reverse().join("");
